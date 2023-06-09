685ff4202df5e7aaba95c89cd30b7ec288db7f57
Alejandro Covarrubias <alex_covag@icloud.com>
2023-06-09 14:07:41 -0600

    better gradle kotlin generation

--- converter.js
+++ converter.js
@@ -1,66 +1,27 @@
 /**
  * Created by sbernstein on 4/8/15.
  */
-function parseAndGenerate(){
+function parseAndGenerate() {
     var text = document.getElementById('input').value;
 
     var parser = new DOMParser();
-    var pomXml = parser.parseFromString(text, 'text/xml');
-
-    var propertyList = [];
-    var dependencies = getDependencies(pomXml, propertyList);
-    var properties = getProperties(pomXml, propertyList);
-
-    return properties + dependencies;
-}
-
-function getProperties(pomXml, propertyList){
-    var shouldAddOuterClosure =  pomXml.getElementsByTagName('properties').length;
-    if(!shouldAddOuterClosure){
+    var parsedDeps = parser.parseFromString(text, 'text/xml');
+    var shouldAddOuterClosure = parsedDeps.getElementsByTagName('dependencies').length;
+    if (!shouldAddOuterClosure) {
         text = '<root>' + text + '</root>';
-        pomXml = parser.parseFromString(text, 'text/xml');
+        parsedDeps = parser.parseFromString(text, 'text/xml');
     }
-
-    var propertyElements = pomXml.getElementsByTagName("properties");
-    var output = "";
-    if(propertyElements != null) {
-        var propertyNodeList = propertyElements[0]. childNodes;
-
-        for(var i = 0; i < propertyNodeList.length; i++) {
-            var propertyNode = propertyNodeList[i];
-            if(propertyNode.namespaceURI == "http://maven.apache.org/POM/4.0.0") {
-                var propertyName = camelCase(propertyNode.tagName);
-                var propertyValue = propertyNode.textContent;
-                if(propertyList.indexOf(propertyName) != -1) {
-                    output += "def " + propertyName + " = \"" + propertyValue + "\"\n"
-                }
-            }
-        }
-    }
-
-    return output;
-}
-
-function camelCase(input) {
-    return input.toLowerCase().replace(/[.-](.)/g, function(match, group1) {
-        return group1.toUpperCase();
-    });
-}
-
-function getDependencies(pomXml, propertyList){
-    var shouldAddOuterClosure =  pomXml.getElementsByTagName('dependencies').length;
-    if(!shouldAddOuterClosure){
-        text = '<root>' + text + '</root>';
-        pomXml = parser.parseFromString(text, 'text/xml');
-    }
-    var depElems = pomXml.getElementsByTagName('dependency');
+    var depElems = parsedDeps.getElementsByTagName('dependency');
     var grDeps = [];
-    for(var i = 0; i < depElems.length; i++) {
+    for (var i = 0; i < depElems.length; i++) {
         var depElem = depElems[i];
         var scopeElems = depElem.getElementsByTagName('scope');
-        var scope = 'implementation';
+
+
+        var scope = document.getElementById('choicebox').value;
+
         if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
-            scope = 'testImplementation';
+            scope = 'testCompile';
         }
         var group = depElem.getElementsByTagName('groupId')[0].innerHTML;
         var artifact = depElem.getElementsByTagName('artifactId')[0].innerHTML;
@@ -69,22 +30,34 @@ function getDependencies(pomXml, propertyList){
         if (versionElems.length) {
             version = versionElems[0].innerHTML;
         }
-        if(version.startsWith("$")) {
-            version = camelCase(version);
-            if(propertyList.indexOf(version) == -1) {
-                propertyList.push(version.substring(2, version.length - 1))
-            }
+
+        var kotlin = document.getElementById('kotlin').checked;
+
+        if (kotlin) {
+            grDeps.push(scope + ' (' + '"' + group + ":" + artifact + ":" + version + '")');
+        } else {
+            grDeps.push(scope + ' ' + '\'' + group + ":" + artifact + ":" + version + '\'');
         }
-        grDeps.push(scope + ' ' + '"' + group + ":" + artifact + ":" + version + '"');
     }
     var grDepsOutput = grDeps.join('\n');
-    if(shouldAddOuterClosure){
+
+    if (depElems.length > 1) {
         grDepsOutput = 'dependencies {\n\t' + grDepsOutput.replace(/\n/g, '\n\t') + '\n}'
     }
+
+    let dsl = document.getElementById('gradleDSL').checked;
+
+    if (dsl && depElems.length > 0) {
+        grDepsOutput = "repositories {\n" +
+            "    mavenCentral()\n" +
+            "    mavenLocal()\n" +
+            "}\n\n" + grDepsOutput
+    }
+
     return grDepsOutput;
 }
 
-function convert(){
+function convert() {
     var ok = document.getElementById('success');
     var empty = document.getElementById('empty');
     var fail = document.getElementById('failed');
@@ -94,14 +67,13 @@ function convert(){
     var output = document.getElementById('output');
     try {
         output.value = parseAndGenerate();
-        if(output.value){
+        if (output.value) {
             ok.hidden = false;
             output.select();
         } else {
             empty.hidden = false;
         }
-    }
-    catch (err){
+    } catch (err) {
         output.value = '';
         fail.hidden = false;
     }
