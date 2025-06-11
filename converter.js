2cf353ce28744714f08fc6c8c24fbc351ad194db
Tripleo <tripleo@tripleo.milkyway>
2025-06-11 19:53:09 -0400

    Simpify

--- converter.js
+++ converter.js
@@ -49,103 +49,6 @@ function camelCase(input) {
     });
 }
 
-function getProperties(pomXml, propertyList){
-    var shouldAddOuterClosure =  pomXml.getElementsByTagName('properties').length;
-    if(!shouldAddOuterClosure){
-        text = '<root>' + text + '</root>';
-        pomXml = parser.parseFromString(text, 'text/xml');
-    }
-
-    var propertyElements = pomXml.getElementsByTagName("properties");
-    var output = "";
-    if(propertyElements != null) {
-        var propertyNodeList = propertyElements[0]. childNodes;
-
-        for(var i = 0; i < propertyNodeList.length; i++) {
-            var propertyNode = propertyNodeList[i];
-            if(propertyNode.namespaceURI == "http://maven.apache.org/POM/4.0.0") {
-                var propertyName = camelCase(propertyNode.tagName);
-                var propertyValue = propertyNode.textContent;
-                if(propertyList.indexOf(propertyName) != -1) {
-                    output += "def " + propertyName + " = \"" + propertyValue + "\"\n"
-                }
-            }
-        }
-    }
-
-    return output;
-}
-
-function camelCase(input) {
-    return input.toLowerCase().replace(/[.-](.)/g, function(match, group1) {
-        return group1.toUpperCase();
-    });
-}
-
-function getDependencies(pomXml, propertyList){
-    var shouldAddOuterClosure =  pomXml.getElementsByTagName('dependencies').length;
-    if(!shouldAddOuterClosure){
-        text = '<root>' + text + '</root>';
-        pomXml = parser.parseFromString(text, 'text/xml');
-    }
-    var depElems = pomXml.getElementsByTagName('dependency');
-    var grDeps = [];
-    for(var i = 0; i < depElems.length; i++) {
-        var depElem = depElems[i];
-        var scopeElems = depElem.getElementsByTagName('scope');
-        //var scope = 'implementation';
-        //if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
-        //    scope = 'testImplementation';
-        //}
-
-
-        var scope = document.getElementById('choicebox').value;
-
-        if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
-            scope = 'testCompile';
-        }
-
-        var group = depElem.getElementsByTagName('groupId')[0].innerHTML;
-        var artifact = depElem.getElementsByTagName('artifactId')[0].innerHTML;
-        var versionElems = depElem.getElementsByTagName('version');
-        var version = '*';
-        if (versionElems.length) {
-            version = versionElems[0].innerHTML;
-        }
-        if(version.startsWith("$")) {
-            version = camelCase(version);
-            if(propertyList.indexOf(version) == -1) {
-                propertyList.push(version.substring(2, version.length - 1))
-            }
-        }
-                var kotlin = document.getElementById('kotlin').checked;
-
-                if (kotlin) {
-                    grDeps.push(scope + ' (' + '"' + group + ":" + artifact + ":" + version + '")');
-                } else {
-                    grDeps.push(scope + ' ' + '\'' + group + ":" + artifact + ":" + version + '\'');
-                }
-
-//        grDeps.push(scope + ' ' + '"' + group + ":" + artifact + ":" + version + '"');
-    }
-    var grDepsOutput = grDeps.join('\n');
-    if (depElems.length > 1) {
-//    if(shouldAddOuterClosure){
-        grDepsOutput = 'dependencies {\n\t' + grDepsOutput.replace(/\n/g, '\n\t') + '\n}'
-    }
-
-    let dsl = document.getElementById('gradleDSL').checked;
-
-    if (dsl && depElems.length > 0) {
-        grDepsOutput = "repositories {\n" +
-            "    mavenCentral()\n" +
-            "    mavenLocal()\n" +
-            "}\n\n" + grDepsOutput
-    }
-
-    return grDepsOutput;
-}
-
 function getDependencies2(text, parser, pomXml, propertyList){
     var shouldAddOuterClosure =  pomXml.getElementsByTagName('dependencies').length;
     if(!shouldAddOuterClosure){
@@ -161,6 +64,14 @@ function getDependencies2(text, parser, pomXml, propertyList){
         if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
             scope = 'testImplementation';
         }
+                var scope2 = document.getElementById('choicebox').value;
+console.log('scope = '+scope);
+console.log('scope2 = '+scope2);
+        if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
+            scope3 = 'testCompile';
+        }
+console.log('scope3 = '+scope3);
+
         var group = depElem.getElementsByTagName('groupId')[0].innerHTML;
         var artifact = depElem.getElementsByTagName('artifactId')[0].innerHTML;
         var versionElems = depElem.getElementsByTagName('version');
@@ -174,13 +85,41 @@ function getDependencies2(text, parser, pomXml, propertyList){
                 propertyList.push(version.substring(2, version.length - 1))
             }
         }
-        grDeps.push(scope + ' ' + '"' + group + ":" + artifact + ":" + version + '"');
+                        var kotlin = document.getElementById('kotlin').checked;
+
+                        if (kotlin) {
+                            grDeps.push(scope + ' (' + '"' + group + ":" + artifact + ":" + version + '")');
+                        } else {
+                            grDeps.push(scope + ' ' + '\'' + group + ":" + artifact + ":" + version + '\'');
+                        }
+
+
+//        grDeps.push(scope + ' ' + '"' + group + ":" + artifact + ":" + version + '"');
     }
     var grDepsOutput = grDeps.join('\n');
+/*
     if(shouldAddOuterClosure){
         grDepsOutput = 'dependencies {\n\t' + grDepsOutput.replace(/\n/g, '\n\t') + '\n}'
     }
     return grDepsOutput;
+*/
+
+        if (depElems.length > 1) {
+    //    if(shouldAddOuterClosure){
+            grDepsOutput = 'dependencies {\n\t' + grDepsOutput.replace(/\n/g, '\n\t') + '\n}'
+        }
+
+        let dsl = document.getElementById('gradleDSL').checked;
+
+        if (dsl && depElems.length > 0) {
+            grDepsOutput = "repositories {\n" +
+                "    mavenCentral()\n" +
+                "    mavenLocal()\n" +
+                "}\n\n" + grDepsOutput
+        }
+
+        return grDepsOutput;
+
 }
 
 function convert() {
